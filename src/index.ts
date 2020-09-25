65aabfbc9c00601f10a9834d5b88885de6152aa8
Kristy Miller <kristy@goodmoney.com>
2020-09-25 12:38:39 -0500

    feat: add basic logs and workflow to test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello from the GitHub Action!');
+import * as core from '@actions/core';
+
+async function run(): Promise<void> {
+  console.log('Hello from your GitHub action!');
+  try {
+    console.log('core.getInput(key)', core.getInput('key'));
+    console.log('core.getInput(schema-var)', core.getInput('schema-variant'));
+
+    core.setOutput('time', new Date().toTimeString());
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
