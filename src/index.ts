2977a9729812d14972a7188738b368586c111c47
Kristy Miller <kristy@goodmoney.com>
2020-09-25 13:22:12 -0500

    feat: add basic apollo server to test against

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log('Hello');
+import { gql, ApolloServer } from 'apollo-server';
+
+interface Movie {
+  name: string;
+  year: number;
+}
+
+const typeDefs = gql`
+  type Movie {
+    name: String!
+    year: Int!
+  }
+
+  type Query {
+    movies: [Movie]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: (): Movie[] => ([
+      { name: 'Jurassic Park', year: 1993 },
+      { name: 'The Lost World', year: 1997 },
+      { name: 'Jurassic Park III', year: 2001 },
+      { name: 'Jurassic World', year: 2015 },
+      { name: 'Jurassic World: Fallen Kingdom', year: 2018 },
+    ]),
+  },
+};
+
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+});
+
+server.listen({ port: 3000 }, () => {
+  console.log('🦖 Apollo Server running at port: 3000');
+});
