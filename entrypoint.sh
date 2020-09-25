8a2d00bd703aea35f1bf12623e8fab2230bdf294
Kristy Miller <kristy@goodmoney.com>
2020-09-25 14:51:48 -0500

    feat: use env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh -l
 
+ARG APOLLO_KEY=$APOLLO_KEY
+ENV APOLLO_KEY=$APOLLO_KEY
+
 ACTION=$1
-APOLLO_KEY=$2
+APOLLO_KEY_INPUT=$2
 SCHEMA_VARIANT=$3
 
 echo "Beginning $ACTION with Apollo Studio... 🚀 "
+
+echo "APOLLO_KEY: $APOLLO_KEY"
+echo "APOLLO_KEY_INPUT: $APOLLO_KEY_INPUT"
 
 echo "$ACTION, $APOLLO_KEY, $SCHEMA_VARIANT"
 
@@ -19,22 +25,9 @@ fi
 if ! command -v apollo &> /dev/null
 then
   echo "installing apollo-tooling"
-  npm install -g apollo
-  apollo -v
+  # npm install -g apollo
+  # apollo -v
 fi
-
-if [ $ACTION = "validate" ]
-then
-  $COMMAND_PREFIX=apollo service:check
-elif [ $ACTION = "publish" ]
-then
-  $COMMAND_PREFIX=apollo service:publish
-else
-  echo "action not supported.. exiting."
-  exit
-fi
-
-echo "COMMAND_PREFIX $COMMAND_PREFIX"
 
 time=$(date)
 echo "::set-output name=time::$time"
