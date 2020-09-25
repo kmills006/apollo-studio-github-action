aa9fe56f4645cce5ff3d72d1daa35b76500bfee2
Kristy Miller <kristy@goodmoney.com>
2020-09-25 13:47:59 -0500

    feat: check for required packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/sh -l
 
-echo "Hello $1, $2, $3"
+ACTION=$1
+APOLLO_KEY=$2
+SCHEMA_VARIANT=$3
+
+echo "\033[1;32mBeginning $ACTION with Apollo Studio... 🚀 \033[0m"
+
+echo "$ACTION, $APOLLO_KEY, $SCHEMA_VARIANT"
+
+if ! command -v npm &> /dev/null
+then
+    echo "NPM not installed"
+    exit
+fi
+
+if ! command -v apollo &> /dev/null
+then
+    echo "apollo-tooling not installed"
+    exit
+fi
+
 time=$(date)
 echo "::set-output name=time::$time"
