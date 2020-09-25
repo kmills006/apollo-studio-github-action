20e1557247da386af3420253a21826bac1a852bd
Kristy Miller <kristy@goodmoney.com>
2020-09-25 13:13:32 -0500

    feat: remove action deps

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-import core from '@actions/core';
-
-async function run(): Promise<void> {
-  console.log('Hello from your GitHub action!');
-  try {
-    const key = core.getInput('key');
-    const schemaVariant = core.getInput('schema-variant');
-
-    console.log('key', key);
-    console.log('schemaVariant', schemaVariant);
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-run();
+console.log('Hello');
